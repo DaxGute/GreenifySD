04b3f52fa6fbdf6c443bb37cb5306a8798514e55
DaxGute <daxton@gutekunst.com>
2022-03-20 18:13:43 -0700

    pause

--- greenify/src/components/map.svelte
+++ greenify/src/components/map.svelte
@@ -13,7 +13,8 @@
         var map = new mapboxgl.Map({
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v11',
-            maxBounds: bounds
+            maxBounds: bounds,
+            attributionControl: false,
         });
     })
 </script>
