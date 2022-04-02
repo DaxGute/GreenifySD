44bcafd8049ea3b1c32dbdd3aa2645453293b38d
DaxGute <daxton@gutekunst.com>
2022-04-02 18:22:37 -0400

    serviceKeyStuff

--- greenify_server/server.js
+++ greenify_server/server.js
@@ -16,10 +16,12 @@ require("firebase/firestore");
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
+const serviceAccount = require('./serviceAccountKey.json');
+
 initializeApp({
-   credential: applicationDefault()
- });
- 
+  credential: cert(serviceAccount)
+});
+
 const db = getFirestore();
 
 
